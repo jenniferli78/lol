13675088f1d23b5ed8bffdbbf9f493f9f7c75f3f
jenniferli78 <jenniferli.email@yahoo.com>
2014-02-09 07:20:32 -0500

    commoonnnnn

---
+++ Documents/Hack/lol/app/controllers/static_pages_controller.rb
@@ -0,0 +1,29 @@
+class StaticPagesController < ApplicationController
+  def home
+  end
+
+  def help
+  end
+
+  def test
+  end
+
+  def Soraka
+  end
+
+  def NuNu
+  end
+
+  def ChoGath
+  end
+
+  def Kogmaw
+  end
+
+  def KhaZix
+  end
+
+  def Malzahar
+  end
+
+end
---
+++ Documents/Hack/lol/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Lol::Application.config.secret_key_base = '8126dfc6357a83c752406299f270876cdab523c4c6a709b96fd28186a04c372c6ff8ac343ddfeeef1edd58405328026ff18a9ae23a5cf411535394a1e63abf45'
